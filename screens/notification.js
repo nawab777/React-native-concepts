a8b20ad735e21d9e17e5c1885615a6b0130bbfbe
naeab777 <isharibrana7@gmail.com>
2022-04-25 11:48:55 +0500

    petscree

--- screens/notification.js
+++ screens/notification.js
@@ -1,28 +1,50 @@
-import { SafeAreaView, ScrollView, Text, View ,Image, ImageBackground,StyleSheet, TextInput, Button, Touchable,TouchableOpacity} from 'react-native'
+import { SafeAreaView, ScrollView, Text, View ,Image, ImageBackground,StyleSheet, TextInput, Button, Touchable,TouchableOpacity,Pressable,Touchablewithoutfeedback} from 'react-native'
 import React, { Component } from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import Entypo from 'react-native-vector-icons/Entypo'
 import AntDesign from 'react-native-vector-icons/AntDesign'
-import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import Feather from 'react-native-vector-icons/Feather'
+
+// import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 import Octicons from 'react-native-vector-icons/Octicons'
   export default function Notifications({navigation}){
    return(
+<ScrollView> 
 
-
-    <View style={styles.Container}>
+  <View style={{backgroundColor:'white',height:hp(100)}}>
+   <View style={styles.Container}>
       <View style={styles.coverphoto}>
-     
-
+        {/* page header elments  */}
+           <ImageBackground source={require('../assets/data4.jpg')} style={{flex:1}}>
+           
+      <View style={{flexDirection:'row',width:wp(100),justifyContent:'space-around',padding:6}}>
+        <Pressable hitSlop={{top:2,left:2,right:2,bottom:2}}>
+          <AntDesign name='left' size={23} color='white'/>
+           </Pressable>
+  <Text style={{color:'white',fontSize:18,fontWeight:'bold'}}>Edit Vendor Profile</Text>
+  <Pressable hitSlop={{top:2,left:2,right:2,bottom:2}} android_ripple={{color:'black'}} style={{backgroundColor:'white',padding:2,borderRadius:6}}>
+    <AntDesign name='setting' size={24} color='black'/> 
+    </Pressable>
+      </View>
+      </ImageBackground>
+ 
       </View>
       {/* lower portion under user  coverphoto */}
+      
       <View style={styles.lowerportion}>
         {/* whole box under lower portion */}
      <View style={styles.profileinfo}> 
      {/* profile dp here below */}
+     <TouchableOpacity>
        <View style={styles.profileimage}>
-         <Image source={require('../assets/data4.jpg')}  style={{resizeMode:"stretch",width:wp(19),height:hp(9)}}/>
-
+                
+           <Image source={require('../assets/data4.jpg')}  style={{resizeMode:"stretch",width:wp(19),height:hp(9),borderRadius:9}}/>
+        
+ 
+        
        </View>
+       </TouchableOpacity>
        {/* //about profile  */}
        <View style={styles.profiletext}>
          <Text style={{fontSize:20,alignSelf:'center',marginTop:4,fontWeight:'bold',color:'black'}}>Eco Dog Care</Text>
@@ -36,31 +58,67 @@ import Octicons from 'react-native-vector-icons/Octicons'
            <View style={styles.mainbuttons}>
 
         <TouchableOpacity  style={styles.like}><AntDesign name='like2' size={15} color='white'/><Text style={{color:'white',marginLeft:4}}>like</Text></TouchableOpacity>
-        <TouchableOpacity  style={[styles.like,styles.like2]}><Octicons name='report' size={15} color='#1e90ff'/><Text style={{color:'#1e90ff',marginLeft:4}}>Report</Text></TouchableOpacity>
+        <TouchableOpacity  style={[styles.like,styles.like2]}><Octicons name='report' size={15} color='#03B7EC'/><Text style={{color:'#03B7EC',marginLeft:4}}>Report</Text></TouchableOpacity>
            </View>
          </View>
      
     </View>
+    {/* navigation from buisness profile */}
+    <View style={{width:wp(90),backgroundColor:'lightgray',borderRadius:12,marginTop:10,padding:9,alignSelf:'center',flexDirection:'row'}}>
+   {/* Gallery */}
+   <TouchableOpacity >
+    <View style={{flexDirection:'column',width:wp(22.5)}}>
+    <FontAwesome5 name='images' size={20} color='black'style={{alignSelf:'center'}}/>
+     <Text style={{alignSelf:'center'}}>Gallery</Text>
+    </View>
+    </TouchableOpacity>
+    <TouchableOpacity>
+     <View  style={{flexDirection:'column',width:wp(22.5)}}>
+  <Feather  name='tool' size={20} color='black' style={{alignSelf:'center'}}/>
+  <Text style={{alignSelf:'center'}}>Services</Text>
+
+     </View>
+     </TouchableOpacity>
+     <TouchableOpacity>
+      <View  style={{flexDirection:'column',width:wp(22.5)}}>
+       <Entypo name='info-with-circle' size={20} color='black' style={{alignSelf:'center'}}/>
+       <Text style={{alignSelf:'center'}}>About</Text>
+      </View>
+      </TouchableOpacity>
+      <TouchableOpacity >
+      <View style={{width:wp(22.5)}}>
+        <Entypo name='dots-three-vertical' size={28} color='black' style={{alignSelf:'center',padding:4}}/>
+        
+      </View>
+      </TouchableOpacity>
+    </View>
+    
      </View>
 
     </View>
-   
+    
+    </View>
+    </ScrollView>
+
    )
    
 
   }
   const styles=StyleSheet.create({
     Container:{
-     height:hp(50),
+     height:hp(55),
      width:wp(100),
-     backgroundColor:"brown"
+     
 
     },
     coverphoto:{
-       flex:0.8
+       flex:0.8,
+      //  justifyContent:'center',
+      //  alignItems:'center',
+       
   
     },like:{
-      backgroundColor:'#1e90ff',
+      backgroundColor:'#03B7EC',
       width:wp(35),
       height:hp(4),
       borderRadius:8,
@@ -70,7 +128,7 @@ import Octicons from 'react-native-vector-icons/Octicons'
       
     },like2:{
       backgroundColor:'white',
-      borderColor:'#1e90ff',
+      borderColor:'#03B7EC',
       borderWidth:1
     }
     ,
